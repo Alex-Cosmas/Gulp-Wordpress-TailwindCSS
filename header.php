2d5eecc90746dee4c2955c56ad8ca2261ec2bff1
AlexCsmas <2alexcosmas@gmail.com>
2020-01-09 17:05:47 +0300

    Gulp Setup in WP using TailwindCSS
    Guided by the PHMU guys. See the link below for a better understanding.
    https://www.phmu.de/blog/setting-up-tailwind-css-in-a-wordpress-theme-using-gulp-js/
    
    A few caveats though, watch out to for closing tags ' '
    
    Replace where needed
    
    gulp.task( ‘styles’. () => {
      return gulp
        .src( config.stlyeSRC, { allowEmpty: true })
        // ...
        .pipe( postcss([
          tailwindcss( './path/to/your/tailwind.config.js' ),
          require( 'autoprefixer' )
        ]) )
        // ...
    });
    // ...
    gulp.task( ‘stylesRTL’. () => {
      return gulp
        .src( config.stlyeSRC, { allowEmpty: true })
        // ...
        .pipe( postcss([
          tailwindcss( './path/to/your/tailwind.config.js' ),
          require( 'autoprefixer' )
        ]) )
        // ...
    });
    // ...
    
    const postcss = require( 'gulp-postcss' );
    const tailwindcss = require( 'tailwindcss' );

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The header for our theme
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page: Title </title>
+    <link rel="stylesheet" href="/public/build/tailwind.css">
+
+</head>
+<body class=" flex flex-col min-h-screen font-body">
+    
+    
+    
+    <section id="header">
+        <!-- Top Nav -->
+        <div class="top-nav bg-primary text-white font-medium p-3">
+            <div class="container mx-auto px-16">
+                <div class="flex justify-between">
+                        <div class="flex items-center">
+                            <p>Language:</p><span class="item-center p-1 ml-3 bg-optional text-white rounded-sm"><p class="">Eng</p></span>
+                        </div>
+                        <div class="flex">
+                            <div class="pr-10">+212 (0)5 22 94 41 95</div>
+                            <div class="">Mon - Thur: 8:30am - 5:30pm | Fri: 8:30am -3:00pm</div>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        
